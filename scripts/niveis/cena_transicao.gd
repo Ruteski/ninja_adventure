647d4a6a0aa1f034ef04f02363d00c0a14e8dd98
Ruteski <lruteski@gmail.com>
2026-06-14 14:10:41 -0300

    Criado titulo animado do nivel que estamos

--- scripts/niveis/cena_transicao.gd
+++ scripts/niveis/cena_transicao.gd
@@ -1,18 +1,24 @@
 class_name CenaTransicao extends CanvasLayer
 
 var path_cena_alvo: String = ""
+var titulo_cena_alvo: String = "Vilarejo"
 
 @export var animador: AnimationPlayer
+@export var titulo: Label
+@export var animador_titulo: AnimationPlayer
 
 
-func mudar_cena(cena_alvo: String) -> void:
+func mudar_cena(cena_alvo: String, _titulo_cena: String) -> void:
 	path_cena_alvo = cena_alvo
+	titulo_cena_alvo = _titulo_cena
 	animador.play('fade_in')
 	
 	
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == 'fade_out':
 		get_tree().call_group('personagem', 'congelar', false)
+		titulo.text = titulo_cena_alvo
+		animador_titulo.play('exibir_titulo')
 		
 	if anim_name == 'fade_in':
 		# fazendo dessa forma por causa desse erro:
@@ -20,6 +26,7 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		# physics callback is not allowed and will cause undesired behavior. 
 		# Remove with call_deferred() instead. Player tem fisica, 
 		# get_tree().call_deferred("change_scene_to_file", _cena_alvo)
+		
 		# antes era assim
 		get_tree().change_scene_to_file(path_cena_alvo)
 		animador.play('fade_out')
--- scripts/niveis/portal.gd
+++ scripts/niveis/portal.gd
@@ -1,12 +1,13 @@
 class_name Portal extends Area2D
 
 @export var _cena_alvo: String = ""
+@export var _titulo_alvo: String = ""
 @export var _posicao_alvo: Vector2
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PersonagemBase:
 		body.congelar(true)
 		gerenciador_portais.posicao_alvo = _posicao_alvo
-		cena_transicao.mudar_cena(_cena_alvo)
+		cena_transicao.mudar_cena(_cena_alvo, _titulo_alvo)
 		
 		
