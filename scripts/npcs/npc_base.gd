9754b25de240686f4cbf880d14d8ce8fed55056e
Ruteski <lruteski@gmail.com>
2026-06-16 18:27:46 -0300

    Criado botao de fala e sistema base de dialogos

--- scripts/npcs/npc_base.gd
+++ scripts/npcs/npc_base.gd
@@ -17,10 +17,16 @@ enum Npcs {
 }
 
 var _direcao: Vector2 = Vector2.ZERO
+var _personagem_no_alcanse: bool = false
 
 @export var _npc_selecionado: Npcs
 @export var _tempo_direcao: Timer
 @export var _tempo_mudar_direcao: float = 5
+@export var _nome_npc: String = ""
+
+#@export var _indicador_fala: Sprite2D
+#ou assim
+@onready var _indicador_fala: Sprite2D = $IndicadorFala
 
 
 func _ready() -> void:
@@ -35,6 +41,12 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	# se tiver personagem no alcance, ele nao se movimenta
+	if _personagem_no_alcanse:
+		velocity = Vector2.ZERO
+		_animar()
+		return
+		
 	velocity = _direcao * 32.0
 	move_and_slide()
 	
@@ -54,19 +66,46 @@ func _physics_process(_delta: float) -> void:
 		
 	if _direcao.y < 0:
 		_prefixo_animacao = "_cima"
-
+	
 	_animar()
 
 
 func _on_tempo_direcao_timeout() -> void:
+	_tempo_mudar_direcao = randi_range(1, 6)
+	_tempo_direcao.start(_tempo_mudar_direcao)	
+	
+	# se tiver personagem no alcance, ele nao se movimenta
+	if _personagem_no_alcanse:
+		return
+		
 	if _direcao == Vector2.ZERO:
 		_direcao = Vector2(
 			randi_range(-1,1), randi_range(-1,1)
 		).normalized()
 		print(_direcao)
-		
 	else:
 		_direcao = Vector2.ZERO
-	
-	_tempo_mudar_direcao = randi_range(1, 6)
-	_tempo_direcao.start(_tempo_mudar_direcao)
+
+
+func _on_area_dialogo_body_entered(body: Node2D) -> void:
+	# msm que fazer a verificacao abaixo, deixei pq usa o self
+	# if body is NpcBase: 
+	#	return 
+	if body is PersonagemBase:
+		if body == self:
+			return 
+			
+		_personagem_no_alcanse = true
+		_indicador_fala.show()
+		print(gerenciador_dialogos.lista_dialogos[_nome_npc])
+
+
+func _on_area_dialogo_body_exited(body: Node2D) -> void:
+	# msm que fazer a verificacao abaixo, deixei pq usa o self
+	if body is NpcBase: 
+		return 
+		
+	# deixado assim pra variar o codigo com a func de entered
+	if body is PersonagemBase:
+		_personagem_no_alcanse = false
+		_indicador_fala.hide()
---
+++ singletons/gerenciador_dialogos.gd
@@ -0,0 +1,13 @@
+class_name GerenciadorDialogos extends Node
+
+var lista_dialogos: Dictionary = {
+	"old_man_vila_inicial": {
+		"foto": "res://assets/actors/character/oldman/faceset.png",
+		"primeiro_dialogo": {
+			
+		},
+		"segundo_dialogo": {
+			
+		}
+	}
+}
